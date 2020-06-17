45387a815288da0b868de6d87912e9eef907afd6
Nick Saravia <nick.saravia@gmail.com>
2020-06-17 10:39:15 -0400

    updated startup to use mvccore and controller with views. Set app to usestaticfiles so that wwwroot content and scripts will be found on view

--- Chunky/Startup.cs
+++ Chunky/Startup.cs
@@ -16,6 +16,8 @@ namespace Chunky
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +28,7 @@ namespace Chunky
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
